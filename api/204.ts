6e5e147a577c4b7045d7bef85cf902991c9cb9d9
LANqed <r13724631421@outlook.com>
2026-07-14 18:27:57 +0800

    add HTTP 204 endpoint

--- api/204.ts
+++ api/204.ts
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-	  res.status(204).end();
+export const config = {
+  runtime: "edge",
+};
+
+export default function handler() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Cache-Control": "no-store",
+    },
+  });
 }
